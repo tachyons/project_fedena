acfc29494fd2cc75cb6aee80a2d90566bb9edc74
Aboobacker MK <aboobackervyd@gmail.com>
2020-06-12 16:46:32 +0530

    Make server up and running

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,7 @@
 require File.join(File.dirname(__FILE__), 'boot')
 
-
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-
 Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
-  config.gem 'declarative_authorization', :source => 'http://gemcutter.org'
-
   config.load_once_paths += %W( #{RAILS_ROOT}/lib )
   config.load_paths += Dir["#{RAILS_ROOT}/app/models/*"].find_all { |f| File.stat(f).directory? }
 
